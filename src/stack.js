f4f025862fe0eaa8d671ae07fd866185c18f29e6
iMaxZorky <mkoprav@gmail.com>
2023-12-11 19:38:10 +0600

    feat: queue task done

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.value = []
+  }
+  push(element) {
+    this.value.push(element)
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.value.pop()
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let vLength = this.value.length
+    let peekIndex = vLength - 1
+    return this.value[peekIndex];
   }
 }
 
